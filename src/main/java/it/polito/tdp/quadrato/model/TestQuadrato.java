fd62a0061aeaa7175ac8d4d32ff92d7db5134142
luki_ <luki_@DESKTOP-JF77T7F.station>
2020-04-22 17:18:32 +0200

    Esercizio terminato

--- src/main/java/it/polito/tdp/quadrato/model/TestQuadrato.java
+++ src/main/java/it/polito/tdp/quadrato/model/TestQuadrato.java
@@ -7,6 +7,7 @@ public class TestQuadrato {
 	public static void main(String[] args) {
 		RisolviQuadrato rs=new RisolviQuadrato(4);
 		
+		
 		List<List<Integer>> soluzioni= rs.quadrati();
 		
 		for(List<Integer> sol: soluzioni) {
